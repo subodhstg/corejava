1dbae947083579c5c8cf207923dac4e9fea9bb04
Subodh Sharma <subodh.stg@gmail.com>
2018-08-20 08:44:36 +0530

    updated App1

--- DateDemo/src/com/movie/dao/App1.java
+++ DateDemo/src/com/movie/dao/App1.java
@@ -2,6 +2,6 @@ package com.movie.dao;
 
 public class App1 {
     public void disp1() {
-        System.out.println("Hello disp1 method");
+        System.out.println("Hello new method method");
     }
 }
